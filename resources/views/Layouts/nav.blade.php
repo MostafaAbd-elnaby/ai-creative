294a60829bb61d2cea202ea7e30f58a3b11f7496
Mostafa-abdo-khattab <mostafa@blue-developments.team>
2024-08-15 23:33:36 +0300

    change

--- resources/views/Layouts/nav.blade.php
+++ resources/views/Layouts/nav.blade.php
@@ -5,7 +5,6 @@
         <div class="logo">
              <a href="{{url('/')}}"><img src={{asset("assets/img/logo.png")}} alt="" class="img-fluid"></a>
         </div>
-
         <nav id="navbar" class="navbar">
             <ul>
                 <li><a class="active" href="{{url('/')}}">{{__("Home")}}</a></li>
@@ -15,14 +14,13 @@
                 {{-- <li><a href="{{route('page.portfolio')}}">{{__("Portfolio")}}</a></li> --}}
                 <li><a href="#contact">{{__("Contact Us")}}</a></li>
             </ul>
-                <div class="col-md-auto" style="padding: 10px 0 10px 30px;">
-                    <select class="changeLang changeBtn">
-                        <option value="en" {{ session()->get('locale') == 'en' ? 'selected' : '' }}>English</option>
-                        <option value="ar" {{ session()->get('locale') == 'ar' ? 'selected' : '' }}>العربية</option>
-                    </select>
-                </div>
+            <div class="col-md-auto" style="padding: 10px 0 10px 30px;">
+                <select class="changeLang changeBtn">
+                    <option value="en" {{ session()->get('locale') == 'en' ? 'selected' : '' }}>English</option>
+                    <option value="ar" {{ session()->get('locale') == 'ar' ? 'selected' : '' }}>العربية</option>
+                </select>
+            </div>
             <i class="bi bi-list mobile-nav-toggle"></i>
-
         </nav>
     </div>
 </header>
